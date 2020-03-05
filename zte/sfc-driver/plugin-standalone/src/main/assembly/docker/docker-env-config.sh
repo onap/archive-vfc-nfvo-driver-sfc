edbd7537bce2b543ccd0f4c1d042e623b75650ae
yangyan <yangyanyj@chinamobile.com>
2020-03-05 11:26:05 +0800

    Remove sudo capability for onap user for VF-C sfc of zte docker
    Change-Id: I6b6a2709992b408f28d63470fcebfce2fe444f44
    Issue-ID: VFC-1640
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- zte/sfc-driver/plugin-standalone/src/main/assembly/docker/docker-env-config.sh
+++ zte/sfc-driver/plugin-standalone/src/main/assembly/docker/docker-env-config.sh
@@ -19,10 +19,6 @@ install_sf(){
 add_user(){
 
 	useradd onap
-	yum -y install sudo
-	chmod u+x /etc/sudoers
-	sed -i '/Same thing without a password/a\onap    ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
-	chmod u-x /etc/sudoers
 	chown onap:onap -R /service
 }
 
